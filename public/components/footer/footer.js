c03c16326dd00f62d5b94cfd817392fecb7e16d0
Ella Ahlstedt <ellaahl02@gmail.com>
2025-05-22 15:27:12 +0200

    added elements to the footer

--- public/components/footer/footer.js
+++ public/components/footer/footer.js
@@ -1,6 +1,7 @@
 function renderFooter(parent) {
     let footerDiv = document.createElement("div");
     footerDiv.id = "footerDiv";
+    footerDiv.innerHTML = "<div class='border'></div>";
 
     parent.appendChild(footerDiv);
 
@@ -10,11 +11,34 @@ function renderFooter(parent) {
 
     let footerInfo = document.createElement("div");
     footerInfo.id = "footerInfo";
-    footerInfo.innerHTML = `<p>Med informationen visuliserad är vår förhoppning att du som stad har fått insikt i vilka bolag du ska välja för just era Ravesfester. De olika bolagen har olika styrkor och svagheter, där vissa bolag kommer matcha era behov mer än andra. Om du är en stad som lägger värde i stor erfarenhet bland produktionsbolagen, så skulle <span class="FHL">Trance AB</span> och <span class="FHL2">Xtas produktioner</span> vara en bra match för er! Är ni däremot mer intresserad av stora dunkande fester i samband med lite större vinster, är <span class="FHL">Trance AB</span> och <span class="FHL2">Neverending AB</span> de bästa bolagen på  det just nu.</p>`;
 
+    let footerText = document.createElement("p");
+    footerText.id = "footerText";
+    footerText.innerHTML = `Med informationen visuliserad är vår förhoppning att du som stad har fått insikt i vilka bolag du ska välja för just era Ravesfester. 
+    De olika bolagen har olika styrkor och svagheter, där vissa bolag kommer matcha era behov mer än andra. 
+    Om du är en stad som lägger värde i stor erfarenhet bland produktionsbolagen, 
+    så skulle <span class="FHLTrance">Trance AB</span> och <span class="FHLXtas">Xtas Produktioner</span> vara en bra match för er! 
+    Är ni däremot mer intresserad av stora dunkande fester i samband med lite större vinster,
+    är <span class="FHLTrance">Trance AB</span> och <span class="FHLNever">Neverending AB</span> de bästa bolagen på det just nu. 
+    <br/><br/> Du som stad får gärna kontakta <img src="./media/images/ravesThatFitFooter.png" alt="Kontakt" /> för vidare samtal och eventuell kontraktskrivning med en av våra bolag. Varmt och lysande välkommen.`;
 
+    let footerContactCon = document.createElement("div");
+    footerContactCon.id = "footerContactCon";
+
+    let footerContactText = document.createElement("p");
+    footerContactText.id = "footerContactText";
+    footerContactText.innerHTML = "Så, vi lägger förhandlingarna på hyllan, <br/> NU SKA DET FESTAS!";
+
+    let footerContactInfo = document.createElement("p");
+    footerContactInfo.id = "footerContactInfo";
+    footerContactInfo.innerHTML = "00 345 562 536 495 <br/> ravethatfit@gmail.com";
 
     footerDiv.appendChild(footerHeadline);
     footerDiv.appendChild(footerInfo);
+    footerInfo.appendChild(footerText);
+    footerInfo.appendChild(footerContactCon);
+    footerContactCon.appendChild(footerContactText);
+    footerContactCon.appendChild(footerContactInfo);
 
+    footerDiv.innerHTML += "<div class='border'></div> <p class='footerLogo'>RAVESthatFIT</p>";
 }
